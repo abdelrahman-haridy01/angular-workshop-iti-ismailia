cbd6e43f943de755179ca906569bd4cacf113cb2
Abdelrahman Haridy <abdelrahman.haridy01@gmail.com>
2022-01-22 15:47:09 +0200

    Example for bootstrap model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,18 @@ export class AppComponent {
   title = 'doctors-sys';
   faCoffee = faCoffee;
   
+  constructor(private modalService: NgbModal) {}
+
   addRecievedItem(value) {
     // console.log(value);
     this.title = value;
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      console.log(result);
+    }, (reason) => {
+      console.log(reason);
+    });
   }
 }
